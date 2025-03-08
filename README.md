ca3ffa90d77fece0b0defaf259931779bacc51a6
Austin Dudas <dudas.austin@gmail.com>
2025-03-08 06:46:22 -0700

    WebGPU Unleashed through 1.1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # WebGPU
 
 Because it seems cool.
+
+## References
+
+- [MDN's WebGPU API Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebGPU_API)
+- [WebGPU Unleashed: A Practical Tutorial](https://shi-yan.github.io/webgpuunleashed/)
--- src/components/WebGpuCanvas/gpu.ts
+++ src/components/WebGpuCanvas/gpu.ts
@@ -11,9 +11,6 @@ async function main() {
   }
   const adapter = await gpu.requestAdapter();
   const device = await adapter.requestDevice();
-  const shaderModule = device.createShaderModule({
-    code: shader,
-  });
 
   const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
   const context: any = canvas.getContext("webgpu")!;
@@ -21,129 +18,60 @@ async function main() {
   context.configure({
     device: device,
     format: gpu.getPreferredCanvasFormat(),
-    alphaMode: "premultiplied",
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    alphaMode: "opaque",
   });
 
-  const vertices = new Float32Array([
-    // Triangle 1: Top-left, bottom-left, top-right
-    -1.0,
-    1.0,
-    0.0,
-    1.0,
-    1.0,
-    0.0,
-    0.0,
-    1.0, // Top-left
-    -1.0,
-    -1.0,
-    0.0,
-    1.0,
-    0.0,
-    1.0,
-    0.0,
-    1.0, // Bottom-left
-    1.0,
-    1.0,
-    0.0,
-    1.0,
-    0.0,
-    0.0,
-    1.0,
-    1.0, // Top-right
+  let colorTexture = context.getCurrentTexture();
+  let colorTextureView = colorTexture.createView();
 
-    // Triangle 2: Bottom-left, bottom-right, top-right
-    -1.0,
-    -1.0,
-    0.0,
-    1.0,
-    0.0,
-    1.0,
-    0.0,
-    1.0, // Bottom-left
-    1.0,
-    -1.0,
-    0.0,
-    1.0,
-    1.0,
-    1.0,
-    1.0,
-    1.0, // Bottom-right
-    1.0,
-    1.0,
-    0.0,
-    1.0,
-    0.0,
-    0.0,
-    1.0,
-    1.0, // Top-right
-  ]);
+  let colorAttachment = {
+    view: colorTextureView,
+    clearValue: { r: 1, g: 0, b: 0, a: 1 },
+    loadOp: "clear",
+    storeOp: "store",
+  };
 
-  const vertexBuffer = device.createBuffer({
-    size: vertices.byteLength, // make it big enough to store vertices in
-    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  const shaderModule = device.createShaderModule({
+    code: shader,
   });
 
-  device.queue.writeBuffer(vertexBuffer, 0, vertices, 0, vertices.length);
+  const pipelineLayoutDesc = { bindGroupLayouts: [] };
+  const layout = device.createPipelineLayout(pipelineLayoutDesc);
 
-  const vertexBuffers = [
-    {
-      attributes: [
-        {
-          shaderLocation: 0, // position
-          offset: 0,
-          format: "float32x4",
-        },
-        {
-          shaderLocation: 1, // color
-          offset: 16,
-          format: "float32x4",
-        },
-      ],
-      arrayStride: 32,
-      stepMode: "vertex",
-    },
-  ];
+  const colorState = {
+    format: "bgra8unorm",
+  };
 
-  const pipelineDescriptor = {
+  const pipelineDesc = {
+    layout,
     vertex: {
       module: shaderModule,
-      entryPoint: "vertex_main",
-      buffers: vertexBuffers,
+      entryPoint: "vs_main",
+      buffers: [],
     },
     fragment: {
       module: shaderModule,
-      entryPoint: "fragment_main",
-      targets: [
-        {
-          format: gpu.getPreferredCanvasFormat(),
-        },
-      ],
+      entryPoint: "fs_main",
+      targets: [colorState],
     },
     primitive: {
       topology: "triangle-list",
+      frontFace: "ccw",
+      cullMode: "back",
     },
-    layout: "auto",
   };
 
-  const renderPipeline = device.createRenderPipeline(pipelineDescriptor);
-  const commandEncoder = device.createCommandEncoder();
-  const clearColor = { r: 0.0, g: 0.5, b: 1.0, a: 1.0 };
+  const pipeline = device.createRenderPipeline(pipelineDesc);
 
   const renderPassDescriptor = {
-    colorAttachments: [
-      {
-        clearValue: clearColor,
-        loadOp: "clear",
-        storeOp: "store",
-        view: context.getCurrentTexture().createView(),
-      },
-    ],
+    colorAttachments: [colorAttachment],
   };
-
+  const commandEncoder = device.createCommandEncoder();
   const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-  passEncoder.setPipeline(renderPipeline);
-  passEncoder.setVertexBuffer(0, vertexBuffer);
-  passEncoder.draw(6);
+  passEncoder.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.draw(3);
   passEncoder.end();
 
   device.queue.submit([commandEncoder.finish()]);
